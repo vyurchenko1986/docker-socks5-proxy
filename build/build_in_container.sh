2710b2c166fd07f9c9ed02671454f0f404694fd4
Valer Yurhenko <vyurcheno198@gmail.om>
2021-02-04 17:34:49 +0200

    add new commit

--- build/build_in_container.sh
+++ build/build_in_container.sh
@@ -5,5 +5,5 @@ cd "$(pwd)/.."
 SQUID_DATA="socks5_data" && \
 docker volume create --name $SQUID_DATA && \
 docker build -t socks5 . && \
-docker run -d -p 1080:1080 --name=docker-socks5-proxy --restart=always -v $SQUID_DATA:/etc/socks5 socks5 && \
+docker run -d -p 1080:1080 -e TZ="Europe/London" --name=docker-socks5-proxy --restart=always -v $SQUID_DATA:/etc/socks5 socks5 && \
 docker system prune -a -f
